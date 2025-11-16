956345f6aed13c547be727faf8ee25c4244b8a7b
Sergei Borovskii <sergei.borovskii@gmail.com>
2025-11-16 19:53:36 +0200

    feat: list items in the db and access them one by one

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  #get "/items", to: "items#index"
+  #post "/items", to: "items#create"
+  root "items#index"
+  resources :items
 end
