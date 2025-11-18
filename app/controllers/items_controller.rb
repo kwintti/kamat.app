81331b06ebc1e65f36f47c637e528659d656bd2a
Sergei Borovskii <sergei.borovskii@gmail.com>
2025-11-18 06:44:53 +0200

    added CRU, D comming next

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,35 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def new 
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to @item
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @item = Item.find(params)
+  end
+
+  def update
+    @product = Item.find(params)
+    if @item.update(item_name)
+      redirect_to @item
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def item_params
+    params.expect(item: [ :name ])
+  end
 end
